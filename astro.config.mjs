dee3585a04106ff2e147117ee6239927bbf5135f
linm1 <linm1@bxscience.edu>
2024-08-21 10:06:47 -0400

    mdx, copy, image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,26 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), mdx()],
   prefetch: {
-    prefetchAll: true,
+    prefetchAll: true
   },
   output: "server",
   // adapter: vercel(),
   adapter: vercel({
     webAnalytics: {
-      enabled: true,
+      enabled: true
     },
     maxDuration: 8,
     // edgeMiddleware: true,
     isr: {
-      expiration: 60 * 60 * 24,
-    },
-  }),
+      expiration: 60 * 60 * 24
+    }
+  })
 });
