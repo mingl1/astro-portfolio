3a7f42da9a4fbdd7ad05cc5b279309cf1fed38d4
linm1 <linm1@bxscience.edu>
2024-08-20 23:58:17 -0400

    fixed ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,12 +6,20 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react({
-    experimentalReactChildren: true
-  })],
+  integrations: [tailwind(), react()],
   prefetch: {
-    prefetchAll: true
+    prefetchAll: true,
   },
   output: "server",
-  adapter: vercel()
+  // adapter: vercel(),
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+    maxDuration: 8,
+    // edgeMiddleware: true,
+    isr: {
+      expiration: 60 * 60 * 24,
+    },
+  }),
 });
